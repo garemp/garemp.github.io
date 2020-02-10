1c27cab2c58b0f82620f6b6214feb98a3ca91603
Jin Yan <59568419+garemp@users.noreply.github.com>
2020-02-10 13:58:14 +0800

    Update list.js

--- list.js
+++ list.js
@@ -2,6 +2,10 @@ $('h1').remove();
 
 $().ready(function(){
     $.getJSON( "./maps.json", function( data ) {
-    $("#text").html(data["version"]);
+    $("#text").html("JSON file version: " + data["version"]);
+    nodes = data["nodes"];
+    $.each(nodes, function(key,value) {
+        $("<tr></tr>").insertAfter("#text");
+    })
   });
 });
